4047bfe70a659f5a31c4897291a219129a965592
Gael Rozario <gaelrozario@gmail.com>
2026-02-18 07:10:07 +0530

    update tfstate file

--- k8s/backend.tf
+++ k8s/backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "local" {
-    path = "/home/gael/workspace/github.com/gael-rozario/lab-tf-state/k8s/terraform.tfstate"
+    path = "/home/gael/workspace/github.com/gael-rozario/lab-tf-state/terraform/k8s/terraform.tfstate"
   }
 }
